b3c9c530c838348cc5d9cbf65bb788f98d81bfc9
Nikunj <mnikunj262@gmail.com>
2026-02-08 01:48:33 +0530

    update: color for dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={`${atypFont.variable} ${garamondFont.variable} ${utsahaFont.variable} antialiased`}
       >
